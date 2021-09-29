d335b1d4b7fde2cc1db4162375858095dff1f3c0
kibinsoo@gmail.com <44609168+soobinism@users.noreply.github.com>
2021-09-28 21:38:03 -0400

    Added ImageList and Pokemon Object

--- app/src/main/java/com/example/assignment3/ImageActivity.kt
+++ app/src/main/java/com/example/assignment3/ImageActivity.kt
@@ -2,6 +2,8 @@ package com.example.assignment3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/assignment3/Pokemon.kt
@@ -0,0 +1,7 @@
+package com.example.assignment3
+
+import androidx.annotation.DrawableRes
+
+data class Pokemon (val id: Int, val name: String, val resourceId: Int){
+
+}
